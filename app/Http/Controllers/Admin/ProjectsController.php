c44d73430238b4bc19bca51b068880265108b501
matteoNapoli42 <matteon4242@gmail.com>
2023-11-10 10:57:01 +0100

    create model, controller and migration

---
+++ app/Http/Controllers/Admin/ProjectsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Project;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    //
+}
